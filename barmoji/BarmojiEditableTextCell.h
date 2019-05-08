eb5cd8e76e97ef3f655aedbddd5e634e9fb7deac
jcarlosperez <carlos@jcarlosperez.me>
2019-05-07 20:16:39 -0400

    add missing preferences text cell files

---
+++ barmoji/BarmojiEditableTextCell.h
@@ -0,0 +1,5 @@
+#import <Preferences/Preferences.h>
+#import <Preferences/PSEditableTableCell.h>
+
+@interface BarmojiEditableTextCell : PSEditableTableCell <UITextViewDelegate, UITextFieldDelegate>
+@end
