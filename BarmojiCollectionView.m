Lessica/Barmoji

commit 87f8691b227bd14b5ce82a1edc9d1aaddbae94d8

latest

BarmojiCollectionView.m
@@ -9,13 +9,16 @@
 #import "Barmoji.h"
 #import "BarmojiCollectionView.h"
 #import "BarmojiHapticsManager.h"
+#import "BarmojiEmojiCell.h"
 
 @interface BarmojiCollectionView () <UICollectionViewDataSource, UICollectionViewDelegate>
 
 @property (assign, nonatomic) UICollectionViewScrollDirection direction;
+@property (assign, nonatomic) BOOL fullWidth;
 @property (assign, nonatomic) BOOL replacingPredictiveBar;
 @property (assign, nonatomic) BOOL useCustomEmojis;
 @property (strong, nonatomic) NSArray *customEmojis;
+@property (strong, nonatomic) NSArray *recentEmojis;
 @property (assign, nonatomic) int emojiPerRow;
 @property (strong, nonatomic) UIKeyboardEmojiKeyDisplayController *emojiManager;
 
@@ -26,8 +29,12 @@
 - (instancetype)initForPredictiveBar:(BOOL)forPredictive; {
     
     NSMutableDictionary *prefs = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/private/var/mobile/Library/Preferences/com.cpdigitaldarkroom.barmoji.plist"];
+
     int emojiSource = ([prefs objectForKey:@"EmojiSource"] ? [[prefs objectForKey:@"EmojiSource"] intValue] : 1);
+
     _direction = ([[prefs objectForKey:@"BarmojiScrollDirection"] ?: @(UICollectionViewScrollDirectionHorizontal) integerValue]);
+    _emojiPerRow = 6;
+    _fullWidth = ([prefs objectForKey:@"BarmojiFullWidthBottom"] ? [[prefs objectForKey:@"BarmojiFullWidthBottom"] boolValue] : NO);
     _replacingPredictiveBar = forPredictive;
     _useCustomEmojis = (emojiSource == 2);
 
@@ -45,6 +52,7 @@
 
     } else {
         self.emojiManager = [[NSClassFromString(@"UIKeyboardEmojiKeyDisplayController") alloc] init];
+        self.recentEmojis = [self.emojiManager recents];
     }
     
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
@@ -53,23 +61,19 @@
     flowLayout.minimumLineSpacing = 0;
 
     if(self = [super initWithFrame:CGRectZero collectionViewLayout:flowLayout]) {
-
+        self.backgroundColor = [UIColor clearColor];
         self.delegate = self;
         self.dataSource = self;
         self.showsVerticalScrollIndicator = NO;
         self.showsHorizontalScrollIndicator = NO;
         self.pagingEnabled = YES;
-        [self registerClass:NSClassFromString(@"UIKeyboardEmojiCollectionViewCell") forCellWithReuseIdentifier:@"EmojiKey"];
-        self.backgroundColor = [UIColor clearColor];
+        [self registerClass:NSClassFromString(@"BarmojiEmojiCell") forCellWithReuseIdentifier:@"kEmojiCellIdentifier"];
         
         [self rotationUpdate:nil];
 
-        // I don't like this at all // there's likely some memory fuckery going on with Barmoji
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-            [self rotationUpdate:nil];
-        });
-
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(rotationUpdate:) name:UIDeviceOrientationDidChangeNotification object:nil];
+
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadLayout:) name:@"barmoji_reloadLayout" object:nil];
 
     }
     return self;
@@ -77,6 +81,10 @@
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+- (void)reloadLayout:(NSNotification *)note {
+    [self reloadData];
 }
 
 - (void)rotationUpdate:(NSNotification *)notification {
@@ -88,7 +96,7 @@
     self.alpha = landscape && !self.replacingPredictiveBar ? 0 : 1;
     // Landscape gets 12 emojis in the predictive bar,
     // Portrait gets 8 in the predictive bar or 6 on the bottom
-    self.emojiPerRow = landscape ? 12 : (self.replacingPredictiveBar ? 8 : 6);
+    self.emojiPerRow = (landscape) ? 12 : ((self.replacingPredictiveBar || self.fullWidth ) ? 8 : 6);
     [self reloadData];
     
 }
@@ -96,30 +104,32 @@
 #pragma mark - UICollectionViewDelegate
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UIKeyboardEmojiCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"EmojiKey" forIndexPath:indexPath];
-
+    BarmojiEmojiCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"kEmojiCellIdentifier" forIndexPath:indexPath];
     if(_useCustomEmojis) {
         NSString *emojiString = self.customEmojis[indexPath.row];
         cell.emoji = [UIKeyboardEmoji emojiWithString:emojiString withVariantMask:0];
     } else {
-        cell.emoji = [self.emojiManager recents][indexPath.row];
+        cell.emoji = _recentEmojis[indexPath.row];
     }
-
-    cell.emojiFontSize = 26;
     return cell;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    int count =  _useCustomEmojis ? self.customEmojis.count : [self.emojiManager recents].count;
+    int count =  _useCustomEmojis ? self.customEmojis.count : _recentEmojis.count;
     return count;
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return CGSizeMake(self.bounds.size.width / self.emojiPerRow, 30);
+    CGFloat useableWidth = collectionView.frame.size.width / self.emojiPerRow;
+    return CGSizeMake(useableWidth, 34);
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
-    return self.replacingPredictiveBar ? UIEdgeInsetsZero : UIEdgeInsetsMake(22, 0, 0, 0);
+    if (self.replacingPredictiveBar || self.fullWidth) {
+        return UIEdgeInsetsZero;
+    }
+
+    return UIEdgeInsetsMake(22, 0, 0, 0);
 }
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {

barmoji/BarmojiRootListController.mm
@@ -73,16 +73,25 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter(void
       }
     }
 
-    specifier = groupSpecifier(@"Locations");
-    setFooterForSpec(@"Bottom Bar: The default Barmoji implementation for iPhone X or devices who have enabled the iPhone X layout. \n\nReplace Predictive Bar: Replaces the text prediction bar with Barmoji, useful for non-iPhone X devices");
+    specifier = groupSpecifier(@"Bottom Bar");
+    setFooterForSpec(@"The default Barmoji implementation for iPhone X or devices who have enabled the iPhone X layout.");
     [mutableSpecifiers addObject:specifier];
 
-    specifier = subtitleSwitchCellWithName(@"Bottom Bar");
+    specifier = subtitleSwitchCellWithName(@"Enabled");
     [specifier setProperty:@"com.cpdigitaldarkroom.barmoji" forKey:@"defaults"];
     setKeyForSpec(@"BarmojiBottomEnabled");
     [mutableSpecifiers addObject:specifier];
 
-    specifier = subtitleSwitchCellWithName(@"Replace Predictive Bar");
+    specifier = subtitleSwitchCellWithName(@"Full Width");
+    [specifier setProperty:@"com.cpdigitaldarkroom.barmoji" forKey:@"defaults"];
+    setKeyForSpec(@"BarmojiFullWidthBottom");
+    [mutableSpecifiers addObject:specifier];
+
+    specifier = groupSpecifier(@"Predictive Bar");
+    setFooterForSpec(@"Replaces the text prediction bar with Barmoji, useful for non-iPhone X devices");
+    [mutableSpecifiers addObject:specifier];
+
+    specifier = subtitleSwitchCellWithName(@"Enabled");
     [specifier setProperty:@"com.cpdigitaldarkroom.barmoji" forKey:@"defaults"];
     setKeyForSpec(@"BarmojiPredictiveEnabled");
     [mutableSpecifiers addObject:specifier];
@@ -181,9 +190,9 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter(void
   [composeViewController setToRecipients:[NSArray arrayWithObjects:@"CP Digital Darkroom <tweaks@cpdigitaldarkroom.support>", nil]];
 
   NSString *product = nil, *version = nil, *build = nil;
-  product = (__bridge_transfer NSString *)MGCopyAnswer(kMGProductType);
-  version = (__bridge_transfer NSString *)MGCopyAnswer(kMGProductVersion);
-  build = (__bridge_transfer NSString *)MGCopyAnswer(kMGBuildVersion);
+  product = (NSString *)MGCopyAnswer(kMGProductType, nil);
+  version = (NSString *)MGCopyAnswer(kMGProductVersion, nil);
+  build = (NSString *)MGCopyAnswer(kMGBuildVersion, nil);
 
   [composeViewController setMessageBody:[NSString stringWithFormat:@"\n\nCurrent Device: %@, iOS %@ (%@)", product, version, build] isHTML:NO];
 
@@ -223,6 +232,7 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter(void
   int feedbackType = [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("BarmojiFeedbackType"), CFSTR("com.cpdigitaldarkroom.barmoji"))) intValue];
   BOOL bottom = [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("BarmojiBottomEnabled"), CFSTR("com.cpdigitaldarkroom.barmoji"))) boolValue];
   BOOL enabled = [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("BarmojiEnabled"), CFSTR("com.cpdigitaldarkroom.barmoji"))) boolValue];
+  BOOL fullWidth = [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("BarmojiFullWidthBottom"), CFSTR("com.cpdigitaldarkroom.barmoji"))) boolValue];
   BOOL predictive = [(id)CFBridgingRelease(CFPreferencesCopyAppValue(CFSTR("BarmojiPredictiveEnabled"), CFSTR("com.cpdigitaldarkroom.barmoji"))) boolValue];
 
   NSDictionary *dictionary = @{

Barmoji.h
@@ -17,6 +17,12 @@
 -(NSArray *)recentEmojis;
 @end
 
+@class BarmojiCollectionView;
+
+@interface TUIPredictionView : UIView // iOS 13 +
+@property (nonatomic,retain) BarmojiCollectionView* barmoji;
+@end
+
 @interface UIKeyboardImpl : UIView
 
 +(id)activeInstance;
@@ -37,13 +43,20 @@
 - (NSArray *)recents;
 @end
 
-@class BarmojiCollectionView;
 @interface UIKeyboardPredictionView : UIView
 @property (nonatomic,retain) BarmojiCollectionView* barmoji;
 @end
 
+@interface UIKeyboardDockItemButton: UIButton
+@end
+
+@interface UIKeyboardDockItem : NSObject
+@property (nonatomic,retain) UIKeyboardDockItemButton * button; 
+@end
+
 @interface UIKeyboardDockView : UIView
 @property (nonatomic,retain) BarmojiCollectionView* barmoji;
+-(id)_keyboardLayoutView;
 @end
 
 @interface UISystemKeyboardDockController : UIViewController

Barmoji.plist
@@ -1,1 +1,1 @@
-{ Filter = { Bundles = ( "com.apple.UIKit", "com.apple.springboard" ); }; }
+{ Filter = { Bundles = ( "com.apple.UIKit", "com.apple.springboard", "com.apple.TextInputUI" ); }; }

Barmoji.xm
@@ -8,25 +8,31 @@
 
 #import "Barmoji.h"
 #import "BarmojiCollectionView.h"
+#import <version.h>
 
 extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter(void);
 
-%group main
-
 BOOL barmojiBottomEnabled = NO;
 BOOL barmojiEnabled = NO;
+BOOL barmojiBottomFullWidth = NO;
 BOOL barmojiPredictiveEnabled = NO;
+
+BOOL showingBarmoji = YES;
+
 
 int barmojiFeedbackType = 7;
 
-%hook UIKeyboardPredictionView
+%group thirteenPlus
+
+%hook TUIPredictionView
+
 %property (retain, nonatomic) BarmojiCollectionView *barmoji;
 - (instancetype)initWithFrame:(CGRect)frame {
+	TUIPredictionView *predictionView = %orig;
 
-	UIKeyboardPredictionView *predictionView = %orig;
 	if(predictionView) {
-
 		if(barmojiEnabled && barmojiPredictiveEnabled) {
+
 			self.barmoji = [[BarmojiCollectionView alloc] initForPredictiveBar:YES];
 			self.barmoji.feedbackType = barmojiFeedbackType;
 			self.barmoji.translatesAutoresizingMaskIntoConstraints = NO;
@@ -36,32 +42,123 @@ int barmojiFeedbackType = 7;
 			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:predictionView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
 			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:30]];
 			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:predictionView attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]];
-		}
 
+			UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(flipSubviewHiddenStatus:)];
+			longPressGesture.minimumPressDuration = 1.0;
+			[self addGestureRecognizer:longPressGesture];
+		}
 	}
 	return predictionView;
 }
 
 - (void)addSubview:(UIView *)subview {
-
 	if(barmojiEnabled && barmojiPredictiveEnabled) {
-		if(subview != self.barmoji) {
-			return;
+		if(![subview isKindOfClass:[BarmojiCollectionView class]]) {
+			subview.hidden = YES;
 		}
 	}
 
 	%orig;
 }
 
--(void)_setPredictions:(NSArray *)arg1 autocorrection:(id)arg2 emojiList:(id)arg3 {
-	if(barmojiEnabled && barmojiPredictiveEnabled) {
-		return;
+- (void)layoutSubviews {
+	%orig;
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"TryReloadTest" object:nil];
+}
+
+-(void)_didRecognizeTapGesture:(id)arg1 {
+	if(barmojiEnabled) {
+		if(barmojiPredictiveEnabled && showingBarmoji) {
+			return;
+		}
 	}
 	%orig;
 }
-%end
+
+%new
+- (void)flipSubviewHiddenStatus:(UILongPressGestureRecognizer *)recognizer {
+	if (recognizer.state == UIGestureRecognizerStateBegan) {
+		showingBarmoji = !showingBarmoji;
+		for (UIView *subview in self.subviews) {
+			subview.hidden = !subview.hidden;
+			subview.userInteractionEnabled = !subview.userInteractionEnabled;
+		}
+	}
+}
+
+%end // TUIPredictionView
+
+%end // thirteenPlus group
+
+%group lessThirteen
+
+%hook UIKeyboardPredictionView
+
+%property (retain, nonatomic) BarmojiCollectionView *barmoji;
+
+- (instancetype)initWithFrame:(CGRect)frame {
+	UIKeyboardPredictionView *predictionView = %orig;
+
+	if(predictionView) {
+		if(barmojiEnabled && barmojiPredictiveEnabled) {
+
+			self.barmoji = [[BarmojiCollectionView alloc] initForPredictiveBar:YES];
+			self.barmoji.feedbackType = barmojiFeedbackType;
+			self.barmoji.translatesAutoresizingMaskIntoConstraints = NO;
+			[predictionView addSubview:self.barmoji];
+
+			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:predictionView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0]];
+			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:predictionView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0]];
+			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:30]];
+			[predictionView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:predictionView attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0]];
+
+			 UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(flipSubviewHiddenStatus:)];
+			 longPressGesture.minimumPressDuration = 1.0;
+			 [self addGestureRecognizer:longPressGesture];
+		}
+	}
+	return predictionView;
+}
+
+- (void)addSubview:(UIView *)subview {
+	if(barmojiEnabled && barmojiPredictiveEnabled) {
+		if(![subview isKindOfClass:[BarmojiCollectionView class]]) {
+			subview.hidden = YES;
+		}
+	}
+	%orig;
+}
+
+-(void)activateCandidateAtPoint:(CGPoint)arg1  {
+	if(barmojiEnabled) {
+		if(barmojiPredictiveEnabled && showingBarmoji) {
+			return;
+		}
+	}
+	%orig;
+}
+
+%new
+- (void)flipSubviewHiddenStatus:(UILongPressGestureRecognizer *)recognizer {
+	if (recognizer.state == UIGestureRecognizerStateBegan) {
+		showingBarmoji = !showingBarmoji;
+		for (UIView *subview in self.subviews) {
+			subview.hidden = !subview.hidden;
+			subview.userInteractionEnabled = !subview.userInteractionEnabled;
+		}
+	}
+}
+
+%end // UIKeyboardPredictionView
+
+%end // lessThirteen group
+
+
+
+%group common
 
 %hook UIKeyboardDockView
+
 %property (retain, nonatomic) BarmojiCollectionView *barmoji;
 
 - (instancetype)initWithFrame:(CGRect)frame {
@@ -74,8 +171,8 @@ int barmojiFeedbackType = 7;
 			self.barmoji.translatesAutoresizingMaskIntoConstraints = NO;
 			[dockView addSubview:self.barmoji];
 
-			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:dockView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:60]];
-			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:dockView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:-50]];
+			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:dockView attribute:NSLayoutAttributeLeading multiplier:1.0 constant: barmojiBottomFullWidth ? 0 : 60]];
+			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:dockView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:barmojiBottomFullWidth ? 0 : -50]];
 			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:60]];
 			[dockView addConstraint:[NSLayoutConstraint constraintWithItem:self.barmoji attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:dockView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:-20]];
 		}
@@ -83,12 +180,28 @@ int barmojiFeedbackType = 7;
 	return dockView;
 }
 
-%end
-%end
+-(void)setLeftDockItem:(UIKeyboardDockItem *)arg1 {
+	if(barmojiBottomFullWidth) { return ;}
+	%orig;
+}
+-(void)setRightDockItem:(UIKeyboardDockItem *)arg1 {
+	if(barmojiBottomFullWidth) { return ;}
+	%orig;
+}
+
+- (void)layoutSubviews {
+	%orig;
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"barmoji_reloadLayout" object:nil];
+}
+
+%end // UIKeyboardDockView
+
+%end // common group
 
 static void loadPrefs() {
 	NSMutableDictionary *prefs = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/private/var/mobile/Library/Preferences/com.cpdigitaldarkroom.barmoji.plist"];
 	barmojiBottomEnabled = ([prefs objectForKey:@"BarmojiBottomEnabled"] ? [[prefs objectForKey:@"BarmojiBottomEnabled"] boolValue] : NO);
+	barmojiBottomFullWidth = ([prefs objectForKey:@"BarmojiFullWidthBottom"] ? [[prefs objectForKey:@"BarmojiFullWidthBottom"] boolValue] : NO);
 	barmojiEnabled = ([prefs objectForKey:@"BarmojiEnabled"] ? [[prefs objectForKey:@"BarmojiEnabled"] boolValue] : NO);
 	barmojiFeedbackType = ([prefs objectForKey:@"BarmojiFeedbackType"] ? [[prefs objectForKey:@"BarmojiFeedbackType"] intValue] : 7);
 	barmojiPredictiveEnabled = ([prefs objectForKey:@"BarmojiPredictiveEnabled"] ? [[prefs objectForKey:@"BarmojiPredictiveEnabled"] boolValue] : NO);
@@ -106,25 +219,43 @@ static void updateSettings(CFNotificationCenterRef center, void *observer, CFStr
 }
 
 %ctor {
+	
 	@autoreleasepool {
+		
+		// check if process is springboard or an application
+		// this prevents our tweak from running in non-application (with UI)
+		// processes and also prevents bad behaving tweaks to invoke our tweak
+		
+		NSArray *args = [[NSClassFromString(@"NSProcessInfo") processInfo] arguments];
+		
+		if (args.count != 0) {
+			NSString *executablePath = args[0];
+			
+			if (executablePath) {
+				NSString *processName = [executablePath lastPathComponent];
+				
+				BOOL isSpringBoard = [processName isEqualToString:@"SpringBoard"];
+				BOOL isApplication = [executablePath rangeOfString:@"/Application"].location != NSNotFound;
+				
+				if (isSpringBoard || isApplication) {
 
-    // check if process is springboard or an application
-    // this prevents our tweak from running in non-application (with UI)
-    // processes and also prevents bad behaving tweaks to invoke our tweak
+					loadPrefs();
 
-    NSArray *args = [[NSClassFromString(@"NSProcessInfo") processInfo] arguments];
-    if (args.count != 0) {
-      NSString *executablePath = args[0];
-      if (executablePath) {
-        NSString *processName = [executablePath lastPathComponent];
-        BOOL isSpringBoard = [processName isEqualToString:@"SpringBoard"];
-        BOOL isApplication = [executablePath rangeOfString:@"/Application"].location != NSNotFound;
-        if (isSpringBoard || isApplication) {
-          loadPrefs();
+					if(IS_IOS_OR_NEWER(iOS_13_0)) {
+						
+						NSBundle* bundle = [NSBundle bundleWithPath:@"/System/Library/PrivateFrameworks/TextInputUI.framework"];
+						if (!bundle.loaded) [bundle load];
+
+						%init(thirteenPlus)
+					} else {
+						%init(lessThirteen)
+					}
+					
 					CFNotificationCenterAddObserver(CFNotificationCenterGetDistributedCenter(), NULL, updateSettings, CFSTR("com.cpdigitaldarkroom.barmoji.settings"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
-					%init(main);
-        }
-      }
-    }
-  }
+					
+					%init(common);
+				}
+			}
+		}
+	}
 }

BarmojiEmojiCell.h
@@ -1,0 +1,14 @@
+//
+//  BarmojiEmojiCell.h
+//  Barmoji
+//
+//  Created by Juan Carlos Perez <carlos@jcarlosperez.me> 11/21/2019
+//  © CP Digital Darkroom <admin@cpdigitaldarkroom.com> All rights reserved.
+//
+
+@class UIKeyboardEmoji;
+@interface BarmojiEmojiCell: UICollectionViewCell
+
+@property (nonatomic,copy) UIKeyboardEmoji *emoji; 
+
+@end

BarmojiEmojiCell.m
@@ -1,0 +1,48 @@
+//
+//  BarmojiEmojiCell.m
+//  Barmoji
+//
+//  Created by Juan Carlos Perez <carlos@jcarlosperez.me> 11/21/2019
+//  © CP Digital Darkroom <admin@cpdigitaldarkroom.com> All rights reserved.
+//
+
+#import "Barmoji.h"
+#import "BarmojiEmojiCell.h"
+
+@interface BarmojiEmojiCell ()
+
+@property (strong, nonatomic) UILabel *emojiLabel;
+
+@end
+
+@implementation BarmojiEmojiCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if(self = [super initWithFrame:frame]) {
+
+        self.contentView.backgroundColor = [UIColor clearColor];
+        self.backgroundColor = [UIColor clearColor];
+
+        _emojiLabel = [[UILabel alloc] initWithFrame: CGRectZero];
+        _emojiLabel.font = [UIFont systemFontOfSize: 24];
+        _emojiLabel.textAlignment = NSTextAlignmentCenter;
+        _emojiLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        [self.contentView addSubview: _emojiLabel];
+
+        [NSLayoutConstraint activateConstraints: @[
+            [_emojiLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor],
+            [_emojiLabel.bottomAnchor constraintEqualToAnchor: self.contentView.bottomAnchor],
+            [_emojiLabel.leadingAnchor constraintEqualToAnchor: self.contentView.leadingAnchor],
+            [_emojiLabel.trailingAnchor constraintEqualToAnchor: self.contentView.trailingAnchor]
+        ]];
+
+    }
+    return self;
+}
+
+- (void)setEmoji:(UIKeyboardEmoji *)emoji {
+    _emoji = emoji;
+    _emojiLabel.text = emoji.emojiString;
+}
+
+@end

Makefile
@@ -1,5 +1,9 @@
-TARGET = :clang:11.2:7.0
-ARCHS = arm64 arm64e
+ifeq ($(SIMJECT),1)
+TARGET = simulator:clang:latest:8.0
+export ARCHS = x86_64
+else
+endif
+
 FINALPACKAGE = 1
 
 include $(THEOS)/makefiles/common.mk
@@ -10,10 +14,9 @@ Barmoji_CFLAGS += -fobjc-arc
 
 include $(THEOS_MAKE_PATH)/tweak.mk
 
-before-stage::
-	find . -name ".DS_Store" -delete
-
 after-install::
 	install.exec "killall -9 SpringBoard"
+
 SUBPROJECTS += barmoji
+
 include $(THEOS_MAKE_PATH)/aggregate.mk

commit fc190580a844b3b7d6341d92610e6878e2b72680

Fix collection view row height
Bug was introduced in 87f8691b

BarmojiCollectionView.m
@@ -121,7 +121,7 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     CGFloat useableWidth = collectionView.frame.size.width / self.emojiPerRow;
-    return CGSizeMake(useableWidth, 34);
+    return CGSizeMake(useableWidth, 30);
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {

barmoji/BarmojiRootListController.mm
@@ -19,7 +19,7 @@ extern "C" CFNotificationCenterRef CFNotificationCenterGetDistributedCenter(void
 
 - (instancetype)init {
     self = [super init];
-        if (self) {
+    if (self) {
         [self createDynamicSpecs];
     }
     return self;